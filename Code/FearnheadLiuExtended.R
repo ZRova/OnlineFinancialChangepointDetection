8616ec584539b8ccb8fe7b2ccd159dc0c390acf9
ZRova <zorie.todo@gmail.com>
2022-02-28 15:40:52 +0000

    testing on disney data, ready for estimation of p

--- Code/FearnheadLiuExtended.R
+++ Code/FearnheadLiuExtended.R
@@ -1,7 +1,16 @@
 #data <- ##SIMULATE NORMAL WITH CHANGE IN MEAN OR ELSE BROWNIAN MOTION WITH SOME SHIFT
-data <- c(rnorm(80, 0, 1), rnorm(120, 8, 1), rnorm(100, 0, 1), rnorm(50, 8, 1))
+#data <- c(rnorm(80, 0, 1), seq(1,4, length.out=120)+rnorm(120, 5, 1), rnorm(100, 0, 1), rnorm(50, 8, 1))
 
+DisneyShares <- read.csv("D:/Zorie/Bristol/#Year3/MathsProject/Examples/DisneySharePrice.csv") # Disney Share Price - I've put this time series in the files section of the Teams channel as well.
 
+Open_Disney <- DisneyShares$Open # The data records the share price at the opening and the closing of trade, as well as the high and low achieved by the share price each day. We focus on the opening price here. (Perhaps you could investigate whether a similar pattern holds for the closing, high and low prices?)
+plot(Open_Disney[1:4000], type = "l")
+
+data <- Open_Disney
+
+# DisneyShares <- read.csv("D:/Zorie/Bristol/#Year3/MathsProject/Examples/DisneySharePrice.csv") # Disney Share Price - I've put this time series in the files section of the Teams channel as well.
+# 
+# data <- DisneyShares$Open
 plot(data, type="l")
 
 #################
@@ -10,8 +19,8 @@ plot(data, type="l")
 
 ##g is the pmf for distance between changepoints
 ##G_k is the sum up to that point
-getG <- function(N){
-  g <- dgeom(1:N, 0.01)
+getG <- function(N, p){
+  g <- dgeom(1:N, p)
   #plot(g, type="l")
   
   G <- rep(0,N)
@@ -38,11 +47,16 @@ calcP <- function(i){
   return(p)
 }#need to pass i as i is used in other places. 
 
+###Updating distance between changepoints in beta distribution.
+# updateG <- function(changepoints, position){
+#   betaNew <- 
+# }
+
 ##checks whether there has been a changepoint
 isChangepoint <- function(row){
   len <- length(row)
   if (len >= minChange){ ##no changepoints before this
-    if ((row[len])>0.9){
+    if ((row[len])>0.95){
       return(TRUE)
     }
   }
@@ -85,6 +99,11 @@ stdvMAD <- function(i, mean){
   }
 }
 
+##checking for change in variance by sorting into bins. 
+varcount <- rep(0,10)
+probRange <- qnorm(seq(0,1, by=0.1), mean, stdv)
+changeVar <- rep(0,10)
+
 ##takes current vector, and returns updated next one, which is longer by 1
 updateModel <- function(row, mean=data[n], stdv=1){
   i <- length(row) # symbolises time since last changepoint. 
@@ -111,18 +130,16 @@ updateModel <- function(row, mean=data[n], stdv=1){
 #INITIALISING RELEVANT VARIBALES#
 #################################
 N <- length(data)
-G <- getG(N)
+G <- getG(N, 0.01)
 minChange <- 10 #Min length between changepoints
-changepoints <- rep(0,10) ##ASSUMED MAX NUMBER OF CHANGEPOINTS, AS APPENDING COPIES ENTIRE ROW ##Needs adjustments
+changepoints <- rep(0,20) ##ASSUMED MAX NUMBER OF CHANGEPOINTS, AS APPENDING COPIES ENTIRE ROW ##Needs adjustments
 numberchangepoints <- 1
 recent_changepoint <- 1
 row <- rep(1,1)
 rollingmean <- 0
 deviation <- c(NULL)
 curdev <- 1
-print(deviation)
 ################################ 
-
 
 ###MAIN
 for (n in 1:(N-1)){
@@ -147,24 +164,33 @@ for (n in 1:(N-1)){
 }
 
 print(changepoints)
+plot(data[1:7000], type = "l")
+abline(v=changepoints, col="red")
 print("done")
 
 ############
 #AT THE END#
 ############
 
-
 ###### TO DO
-#starting again at each timepoint so matrix doesnt get ludicrously large
 
-######## Potential improvements
-### W BROWNIAN,  Median absolute deviation for standard deviation
-#estimating sigma MAD (meadian absolute deviation, robust statistics) sequential hypothesis testing book, likelihood ratio for changing ratio
+######## Potential improvements. CHANGE IN MEAN!
+### W BROWNIAN, 
+#estimating sigma MAD (meadian absolute deviation, robust statistics) sequential hypothesis testing book, 
+#####likelihood ratio for changing ratio
+#####change in variance
 # what data in bins, and see whether equal amounts fall in each bin. If in outer bins, variance grown, if in middle only
-#consider splitting long times of no changepoints, keep 10 a couple so can detect immediate changepoints
-#remove the matrix, and replace with a vector
 
+# optim
+# geometric is a posterior estimator of gap between changepoints. 
+# simulated annealing - choosing parameters, threshold parameter such that detect as many things as possible, with as few mistakes as possible. 
+# - heuristic optimisation - looking for correct parameters and jump around, but as you get closer, you slowly converge to what you think is correct. 
+
+###LOW PRIORITY
+#starting again after x timepoint so matrix doesnt get ludicrously large? only if necessary
+#consider splitting long times of no changepoints, keep 10 a couple so can detect immediate changepoints
 #pointless efficiency improvement, lookup table for P, Too much space added to be worth it
+
 
 #### Learn the amount of time between changepoints rather than assuming 
 # SAFEBAYES (could be bayesian about it)
@@ -176,3 +202,4 @@ print("done")
 #get it to detect the changepoint peak
 #make the matrix a vector
 #MEAN FROM PREV values,
+##Median absolute deviation for standard deviation
